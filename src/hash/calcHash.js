c44d2c446c98d2d2b6d24cf6009af62805fd542a
Roman Lynko <lynko.roman090392@gmail.com>
2024-01-29 23:53:02 +0300

    feat: hash finished

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,18 @@
+import fs from 'fs';
+import path from 'path';
+import crypto from 'crypto'
+
+const file = 'fileToCalculateHashFor.txt';
+
 const calculateHash = async () => {
-    // Write your code here 
+    const filePath = path.resolve(`src/hash/files/${file}`);
+    const stream = fs.createReadStream(filePath);
+    const hash = crypto.createHash('sha256');
+
+    stream.on('data', (data) => {
+        hash.update(data);
+        console.log(hash.digest('hex'));
+    });
 };
 
 await calculateHash();
