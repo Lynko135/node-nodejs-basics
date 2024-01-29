3ec79ea83901e126273ff5c9f1af734180978e2b
Roman Lynko <lynko.roman090392@gmail.com>
2024-01-29 15:30:10 +0300

    fs module ready

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,19 @@
+import fs from 'fs';
+import path from 'path';
+
+const errorMsg = 'FS operation failed';
+const newFolder = 'files_copy';
+const oldFolder = 'files';
+
 const copy = async () => {
-    // Write your code here 
+    const newFolderPath = path.resolve(`src/fs/${newFolder}`);
+    const oldFolderPath = path.resolve(`src/fs/${oldFolder}`);
+
+    try {
+		await fs.promises.cp(oldFolderPath, newFolderPath, { recursive: true, force: false, errorOnExist: true });
+	} catch {
+		throw new Error(errorMsg);
+	}
 };
 
 await copy();
--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,18 @@
+import fs from 'fs';
+import path from 'path';
+
+const errorMsg = 'FS operation failed';
+const fileToRemove = 'fileToRemove.txt';
+
 const remove = async () => {
-    // Write your code here 
+    const fileToRemovePath = path.resolve(`src/fs/files/${fileToRemove}`);
+
+    try {
+        await fs.promises.unlink(fileToRemovePath);
+    }
+    catch {
+        throw new Error(errorMsg);
+    } 
 };
 
 await remove();
