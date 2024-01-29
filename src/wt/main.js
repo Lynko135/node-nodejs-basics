c832f1c444b2a6fae529b8fddd538fb76c82dd0a
Roman Lynko <lynko.roman090392@gmail.com>
2024-01-30 02:51:25 +0300

    feat: wt finished

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,27 @@
+import path from 'path';
+import os from 'os';
+import { Worker } from 'worker_threads';
+
+const start = 10;
+const workerFile = 'worker.js';
+
 const performCalculations = async () => {
-    // Write your code here
+    const workerPath = path.resolve(`src/wt/${workerFile}`)
+    const cpu = os.cpus();
+    const resultPrArr = [];
+
+    for(let i = 0; i < cpu.length; i++) {
+        resultPrArr.push(new Promise((res) => {
+            const worker = new Worker(workerPath, {workerData: start + i});
+
+            worker.on('message', (data) => res({ status: 'resolved', data }));
+			worker.on('error', () => res({ status: 'error', data: null }));
+        }));
+    }
+
+    const finalResult = await Promise.all(resultPrArr);
+
+    console.log(finalResult);
 };
 
 await performCalculations();
